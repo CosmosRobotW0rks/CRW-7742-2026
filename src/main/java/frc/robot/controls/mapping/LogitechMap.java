106695d35aa28deb55301af68fad47834faf5672
Bora <89869983+bora-sy@users.noreply.github.com>
2026-01-29 12:49:32 +0300

    Update button mappings for logitech

--- src/main/java/frc/robot/controls/mapping/LogitechMap.java
+++ src/main/java/frc/robot/controls/mapping/LogitechMap.java
@@ -8,9 +8,9 @@ public final class LogitechMap {
     public static final int AXIS_L2 = 2;
     public static final int AXIS_R2 = 3;
 
-    public static final int BUTTON_LEFT = 1;
-    public static final int BUTTON_DOWN = 2;
-    public static final int BUTTON_RIGHT = 3;
+    public static final int BUTTON_LEFT = 3;
+    public static final int BUTTON_DOWN = 1;
+    public static final int BUTTON_RIGHT = 2;
     public static final int BUTTON_UP = 4;
     public static final int BUTTON_L1 = 5;
     public static final int BUTTON_R1 = 6;
