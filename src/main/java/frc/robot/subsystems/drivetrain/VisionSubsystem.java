11aac9a68df2a2d079ef28ac10e913bca3d03667
Bora <89869983+bora-sy@users.noreply.github.com>
2026-03-08 00:55:19 +0300

    Update VisionSubsystem.java

--- src/main/java/frc/robot/subsystems/drivetrain/VisionSubsystem.java
+++ src/main/java/frc/robot/subsystems/drivetrain/VisionSubsystem.java
@@ -19,16 +19,15 @@ import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.subsystems.drivetrain.swerve.SwerveSubsystem;
 
-// TODO: Implement VisionSubsystem
 public class VisionSubsystem extends SubsystemBase {
 
     PhotonCamera camera = new PhotonCamera("maincam");
 
-    // TODO: Use 2026 field
     public static final AprilTagFieldLayout kTagLayout = AprilTagFieldLayout.loadField(AprilTagFields.k2026RebuiltAndymark);
 
-    public static final Transform3d kRobotToCam = new Transform3d(new Translation3d(0.325, 0.0, 0),
-            new Rotation3d(0, -65, 0));
+    // TODO: Put robot to cam values
+    public static final Transform3d kRobotToCam = new Transform3d(new Translation3d(0, 0.0, 0),
+            new Rotation3d(0, 0, 0));
 
     private PhotonPoseEstimator estimator = new PhotonPoseEstimator(kTagLayout,
             PoseStrategy.MULTI_TAG_PNP_ON_COPROCESSOR, kRobotToCam);
@@ -58,39 +57,17 @@ public class VisionSubsystem extends SubsystemBase {
             Pose3d p3d = visionEst.get().estimatedPose;
             Pose2d p2d = p3d.toPose2d();
 
-            // TODO: Check if rotation is required
             Pose2d rotatedP2d = new Pose2d(p2d.getX(), p2d.getY(), p2d.getRotation().rotateBy(Rotation2d.k180deg));
             swerveDt.addVisionMeasurement(rotatedP2d, visionEst.get().timestampSeconds);
 
-            SmartDashboard.putNumberArray("PHOTONTEST/T3D", new Double[] { p3d.getX(), p3d.getY(), p3d.getZ() });
+            SmartDashboard.putNumberArray("VISION/T3D", new Double[] { p3d.getX(), p3d.getY(), p3d.getZ() });
 
             sucC++;
         }
 
         totalC++;
 
-        SmartDashboard.putNumber("PHOTONTEST/SUCC", sucC);
-        SmartDashboard.putNumber("PHOTONTEST/TOTALC", totalC);
-    }
-
-    void a() {
-
-        var results = camera.getAllUnreadResults();
-
-        if (results.size() == 0)
-            return;
-
-        var res = results.get(results.size() - 1);
-
-        if (!res.hasTargets())
-            return;
-
-        var target = res.getBestTarget();
-
-        Transform3d t3d = target.getBestCameraToTarget();
-
-        SmartDashboard.putNumber("PHOTONTEST/T3D_X", t3d.getX());
-        SmartDashboard.putNumber("PHOTONTEST/T3D_Y", t3d.getY());
-        SmartDashboard.putNumber("PHOTONTEST/T3D_Z", t3d.getZ());
+        SmartDashboard.putNumber("VISION/SUCC", sucC);
+        SmartDashboard.putNumber("VISION/TOTALC", totalC);
     }
 }
