3213639cbb9af87e7ae7b2d8d0ae517cac582356
Bora <89869983+bora-sy@users.noreply.github.com>
2026-03-08 03:27:46 +0300

    Update ApproachPoseCommand.java

--- src/main/java/frc/robot/auto/AutoHelper.java
+++ src/main/java/frc/robot/auto/AutoHelper.java
@@ -16,8 +16,8 @@ public class AutoHelper {
         final Pose2d wp1 = new Pose2d(1.68, 3.744, Rotation2d.kZero);
         final Pose2d wp2 = new Pose2d(0.951, 3.744, Rotation2d.kZero);
 
-        final ApproachPoseConfiguration config1 = new ApproachPoseConfiguration(wp1);
-        final ApproachPoseConfiguration config2 = new ApproachPoseConfiguration(wp2).withMaxSpeed(0.5);
+        final ApproachPoseConfiguration config1 = ApproachPoseConfiguration.fromPose(wp1);
+        final ApproachPoseConfiguration config2 = ApproachPoseConfiguration.fromPose(wp2).withMaxSpeed(0.5);
 
         ApproachPoseCommand cmd1 = new ApproachPoseCommand(swerve, config1);
         ApproachPoseCommand cmd2 = new ApproachPoseCommand(swerve, config2);
